5fc1a998705073dbb1e3f52ce85a59cf4c2438c3
tank <keluarga.azfar@gmail.com>
2023-11-24 14:50:19 +0700

    #

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let clickedyes = false;
 function moveDiv() {
     if(Math.random()<0.3) return changediv();
 
@@ -30,4 +30,50 @@ function changediv(){
         nochoice.classList.remove('enlarge');
         choice.classList.remove('enlarge');
     }, 300); // 
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function yes(){
+    if(clickedyes==true) return;
+    clickedyes=true;
+    var moveableDiv = document.getElementById('notachoice');
+    setTimeout(() => {
+    
+    var screenWidth = window.innerWidth - 100; // Adjust for div width
+    var screenHeight = window.innerHeight - 100; // Adjust for div height
+
+    var randomX = Math.floor(Math.random() * screenWidth);
+    var randomY = Math.floor(Math.random() * screenHeight);
+
+    moveableDiv.style.left = randomX + 'px';
+    moveableDiv.style.top = randomY + 'px';
+    }, 600);
+    setTimeout(() => {
+    
+    moveableDiv.remove();
+  
+
+    }, 2000);
+    var nochoice = document.getElementById('choice1');
+    var headingElement = nochoice.querySelector("p");
+    
+    words = ["... ","‎wh- ","‎what? ","‎huh? ","‎you werent supposed to do that..."]
+    headingElement.textContent = "";
+  for(x=0;x<words.length;x++){
+
+    wordsnow = words[x].split("")
+    for(y=0;y<wordsnow.length;y++){
+      await  sleep(70)
+    headingElement.textContent = headingElement.textContent + wordsnow[y]
+    if(headingElement.textContent.split("").length>10)
+   // console.log(headingElement.textContent.split(""))
+    headingElement.textContent =headingElement.textContent.slice( 1 )
+   }
+   while(wordsnow.length<headingElement.textContent.length){await  sleep(70)
+    headingElement.textContent =headingElement.textContent.slice( 1 )}
+ 
+   await sleep(1200)
+  }
+
 }
