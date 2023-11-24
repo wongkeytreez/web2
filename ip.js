bd423ec048106a907b5e31b47f8ad2bead0c89d9
tank <keluarga.azfar@gmail.com>
2023-11-24 11:15:12 +0700

    #

---
+++ ip.js
@@ -0,0 +1,94 @@
+
+// Define your GitHub repo and file path
+const owner = "wongkeytreez";
+const repo = "New-folder";
+const path = "idk.json";
+
+// Replace with your GitHub personal access token
+const token = "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";
+let maxRetries =3
+let userIP;
+
+function fetchUserIP(maxRetries, retries = 0) {
+  if (retries >= maxRetries) {
+    console.error("Failed to retrieve the user's IP address after multiple attempts.");
+    return;
+  }
+
+  // Fetch the user's IP address
+  fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+      userIP = data.ip;
+
+      if (userIP === undefined) {
+       
+        // Retry after a delay (e.g., 1 second)
+        setTimeout(() => fetchUserIP(maxRetries, retries + 1), 1000);
+      } else {
+        updateGitHubFile();
+      }
+    })
+    .catch(error => {
+    
+      if (retries < maxRetries) {
+       
+        // Retry after a delay (e.g., 1 second)
+        setTimeout(() => fetchUserIP(maxRetries, retries + 1), 1000);
+      }
+    });
+}
+const userAgent = navigator.userAgent;
+
+console.log( userAgent.match(/(\w+)\/(\d+\.\d+\.\d+\.\d+)/)[1])
+
+function updateGitHubFile() {
+  // Make a GET request to get the current file content
+  let location;
+   fetch(`https://ipinfo.io/${userIP}/json?token=ddc68121fce0ee`, {
+    method: "GET",
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      
+     location=data.city+", "+data.region+", "+data.timezone;
+    
+      fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
+    method: "GET",
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+
+    const content = JSON.parse(atob(data.content));
+   
+     
+      content[userIP] = {time:new Date().toString().split(" GMT")[0],location:location,language: navigator.language,browser:userAgent.match(/(\w+)\/(\d+\.\d+\.\d+\.\d+)/)[1],wh:window.screen.width,ww: window.screen.height};
+
+      // Make a PUT request to update the file
+      fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
+        method: "PUT",
+        headers: {
+          Authorization: `token ${token}`,
+        },
+        body: JSON.stringify({
+          message: "Update file",
+          content: btoa(JSON.stringify(content)), // Encode new content as base64
+          sha: data.sha, // Include the original file's SHA
+        }),
+      });
+    })
+    .catch(error => {
+      console.error("Error updating GitHub file:", error);
+    });
+     });
+  
+} 
+
+// Start the process by attempting to fetch the user's IP address with a maximum of 3 retries
+fetchUserIP(3);
